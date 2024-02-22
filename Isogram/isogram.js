7cb822928aa7e37b5588ac71310aa7de57d1d56e
Reagan <rokhanga@gmail.com>
2024-02-22 11:27:13 +0300

    Implement the function

---
+++ Isogram/isogram.js
@@ -0,0 +1,15 @@
+function isIsogram(str) {
+    const newstr = str.toLowerCase();
+
+    const uniqueChars = new Set();
+
+    for(const char of newstr) {
+        if(/^[a-z]&/.test(char)) {
+            if(uniqueChars.has(char)) {
+                return false;
+            }
+            uniqueChars.add(char);
+        }
+    }
+    return true;
+}
